b1442ab18e985c1a8175ee66dd3948921f2a6cd6
arsalan-with-jawaid <159759857+arsalan-with-jawaid@users.noreply.github.com>
2024-03-08 11:31:31 +0000

    Add files via upload

---
+++ allmain.js
@@ -0,0 +1,10 @@
+// 4 Question
+var qoute = 'A person who never made a mistake never tried anything new';
+var aurthor = 'Albert Einstein';
+console.log("".concat(aurthor, " once said,\"").concat(qoute));
+// 5 question
+var qoute = 'A person who never made a mistake never tried anything new';
+var famous_person = "Albert Einstine";
+var message = "".concat(famous_person, " once said, ").concat(qoute);
+console.log(message);
+
---
+++ allmain.ts
@@ -0,0 +1,11 @@
+// 4 Question
+
+let qoute: string='A person who never made a mistake never tried anything new';
+let aurthor: string= 'Albert Einstein'
+console.log(`${aurthor} once said,"${qoute}`);
+
+// 5 question
+let qoute: string='A person who never made a mistake never tried anything new';
+let famous_person="Albert Einstine"
+let message=`${famous_person} once said, ${qoute}`;
+console.log(message);
